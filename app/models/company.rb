7a8d696cb706942d883502aa27b8dd798a81fc61
EC2 Default User <ec2-user@ip-172-31-15-100.ap-northeast-1.compute.internal>
2023-06-25 06:58:08 +0000

    initial commit

---
+++ app/models/company.rb
@@ -0,0 +1,29 @@
+class Company < ApplicationRecord
+ def self.search(search_type, field1, value1, field2, value2)
+  
+      if search_type == 'AND'
+        where("#{field1} = ? AND #{field2} = ?", value1, value2)
+      else
+        where("#{field1} = ? OR #{field2} = ?", value1, value2)
+      end
+    
+ end
+
+  validates :company_code, presence: true 
+  validates :listing_status, presence: true 
+  validates :company_name, presence: true 
+  validates :company_name_kana, presence: true 
+  validates :mail_address, presence: true 
+  validates :address, presence: true 
+  validates :representative, presence: true 
+  validates :representative_kana, presence: true 
+  validates :phone_nubmer, presence: true 
+  validates :earnings_2022, presence: true 
+  validates :profit_2022, presence: true 
+  validates :earnings_2021, presence: true 
+  validates :profit_2021, presence: true 
+  validates :earnings_2020, presence: true 
+  validates :profit_2020, presence: true 
+  
+  belongs_to :user
+end
